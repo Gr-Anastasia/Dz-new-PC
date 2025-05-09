20a67d177109e88358cc7576e19848159a78c428
Anastasiya <nastya@mail.ru>
2025-05-09 13:48:02 +0500

    ДЗ апрель + май

---
+++ Апрель/Дз. Камень, ножницы, бумага/Камень, ножницы, бумага. Григорьева.py
@@ -0,0 +1,36 @@
+import random
+
+choise = {1 : "Камень", 2 : "Бумага", 3 : "Ножницы"}
+
+def get_user_action():
+    user_action = int(input("Выберите число:1 - Камень, 2 - Бумага или 3 - Ножницы: "))
+    return choise[user_action]
+
+def get_computer_action():
+    possible_actions = ["Камень", "Бумага", "Ножницы"]
+    computer_action = random.choice(possible_actions)
+    return computer_action
+
+def sravnenie(user_choise, comp_choise):
+    if user_choise == comp_choise:
+        print("Ничья!")
+    elif user_choise == "Камень" and comp_choise == "Ножницы" or user_choise == "Бумага" and comp_choise == "Камень" or user_choise == "Ножницы" and comp_choise == "Бумага":
+        print("Вы выиграли!")
+    else:
+        print("Вы проиграли!")
+      
+
+def isEnd():
+    finish = input("Завершить игру? да или нет:  ")
+    return finish == "да"
+    
+while True:
+    user_choise = get_user_action()
+    comp_choise = get_computer_action()
+    print("Выбор компьютера: ", comp_choise)
+    print("Выбор пользователя: ", user_choise)
+    sravnenie(user_choise, comp_choise)
+    if isEnd():
+        break
+
+
